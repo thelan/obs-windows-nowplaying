1c61bc2ca3bb2fa8731ed52039620c4a831c2407
Julien Recurt <thelan@servme.fr>
2023-06-09 11:42:02 +0200

    initial GitHub import

---
+++ README.md
@@ -0,0 +1,39 @@
+# OBS Windows Now Playing
+
+This script gather the now playing title from the Windows api.
+
+It updates the current title into a custom data in wize bot streaming tool.
+
+It also can update a local text field to display on stream / debug
+
+## Setup
+
+1. Install [Python for Windows](https://www.python.org/downloads/windows/) (tested with 3.11)
+2. Install winsdk package
+   
+    From a command prompt or powershell run the following command:
+    ```python -m pip install winsdk==1.0.0b9```
+3. Log onto your Wize Bot account and gather the apikey:
+   
+    ![wizebot-apikey](https://github.com/thelan/obs-windows-nowplaying/blob/master/doc/wizebot-apikey.png?raw=true)
+
+   If you don't want to use an online service you can also add a text source to update 
+4. Open OBS and go to scripts
+   
+   ![OBS-add-script](https://github.com/thelan/obs-windows-nowplaying/blob/master/doc/step-add-script.png?raw=true)
+
+   Tell OBS where you have installed python
+
+   ![configure-python](https://github.com/thelan/obs-windows-nowplaying/blob/master/doc/configure-python.png?raw=true)
+
+    Add the script in the src directory to OBS and configure it
+
+   ![OBS-Configure-script](https://github.com/thelan/obs-windows-nowplaying/blob/master/doc/step-config-script.png?raw=true)
+5. Configure Wize Bot to use the Custom data
+    
+   Eg: use the tag in a command response using  `$custom_data(obs-np)`. Replace `obs-np` to match the key you put in the settings
+
+## More info
+
+For more information please read Wize Bot documentation about custom datas:
+https://support.wizebot.tv/docs/api_custom_datas
---
+++ src/wizebot-now-playing.py
@@ -0,0 +1,126 @@
+import obspython as obs
+import asyncio
+
+import urllib.error
+import urllib.parse
+import urllib.request
+
+from winsdk.windows.media.control import GlobalSystemMediaTransportControlsSessionManager as MediaManager
+
+
+# -----
+# User overridable variables
+apikey = ""
+wizebot_field = "obs-np"
+interval = 10
+source_name = ""
+# -----
+
+# -----
+# Internal variables
+last_data = None
+# ------------------------------------------------------------
+
+
+## See https://stackoverflow.com/a/66037406
+async def get_media_info():
+    sessions = await MediaManager.request_async()
+    current_session = sessions.get_current_session()
+    if current_session:  # there needs to be a media session running
+        info = await current_session.try_get_media_properties_async()
+        info_dict = {song_attr: info.__getattribute__(song_attr) for song_attr in dir(info) if song_attr[0] != '_'}
+
+        info_dict['genres'] = list(info_dict['genres'])
+
+        return info_dict
+
+    return
+
+
+def set_wizebot_custom_data(api_key, key, value):
+    url = 'https://wapi.wizebot.tv/api/custom-data/{}/set/{}/{}'.format(api_key, key, urllib.parse.quote(value))
+    try:
+        req = urllib.request.Request(url, method='POST')
+        urllib.request.urlopen(req)
+        obs.script_log(obs.LOG_INFO, "Wise bot updated")
+
+    except urllib.error.URLError as err:
+        obs.script_log(obs.LOG_WARNING, "Error opening URL '" + url + "': " + err.reason)
+        obs.remove_current_callback()
+
+
+def update_text():
+    global apikey, wizebot_field, interval, source_name, last_data
+    try:
+        media_info = asyncio.run(get_media_info())
+    except OSError:
+        obs.script_log(obs.LOG_WARNING, "Failed to get now playing")
+        return
+    now_playing = '{} - {}'.format(media_info['title'], media_info['artist'])
+
+    # Skip update if nothing changed
+    if last_data == now_playing:
+        return
+
+    last_data = now_playing
+
+    obs.script_log(obs.LOG_INFO, "Now playing '" + now_playing + "'")
+    if apikey != '' and wizebot_field != '':
+        set_wizebot_custom_data(apikey, wizebot_field, now_playing)
+
+    source = obs.obs_get_source_by_name(source_name)
+    if source is not None:
+        settings = obs.obs_data_create()
+        obs.obs_data_set_string(settings, "text", now_playing)
+        obs.obs_source_update(source, settings)
+        obs.obs_source_release(source)
+
+
+def refresh_pressed(props, prop):
+    update_text()
+
+
+def script_description():
+    return "Update a now playing variable on Wize bot detecting the song using Windows API.\n\nBy thelan"
+
+
+def script_update(settings):
+    global apikey, wizebot_field, interval, source_name
+
+    apikey = obs.obs_data_get_string(settings, "apikey")
+    wizebot_field = obs.obs_data_get_string(settings, "wizebot_field")
+    interval = obs.obs_data_get_int(settings, "interval")
+    source_name = obs.obs_data_get_string(settings, "source")
+
+    obs.timer_remove(update_text)
+
+    obs.timer_add(update_text, interval * 1000)
+
+
+def script_defaults(settings):
+    global interval, wizebot_field
+    obs.obs_data_set_default_int(settings, "interval", interval)
+    obs.obs_data_set_default_string(settings, "wizebot_field", wizebot_field)
+
+
+def script_properties():
+    props = obs.obs_properties_create()
+
+    obs.obs_properties_add_text(props, "apikey", "Wize Bot API", obs.OBS_TEXT_DEFAULT)
+    obs.obs_properties_add_text(props, "wizebot_field", "Wize Bot Custom Data", obs.OBS_TEXT_DEFAULT)
+    obs.obs_properties_add_int(props, "interval", "Update Interval (seconds)", 5, 3600, 1)
+
+    p = obs.obs_properties_add_list(props, "source", "Text Source", obs.OBS_COMBO_TYPE_EDITABLE,
+                                    obs.OBS_COMBO_FORMAT_STRING)
+    sources = obs.obs_enum_sources()
+    if sources is not None:
+        for source in sources:
+            source_id = obs.obs_source_get_unversioned_id(source)
+            if source_id == "text_gdiplus" or source_id == "text_ft2_source":
+                name = obs.obs_source_get_name(source)
+                obs.obs_property_list_add_string(p, name, name)
+
+        obs.source_list_release(sources)
+
+    obs.obs_properties_add_button(props, "button", "Refresh", refresh_pressed)
+    return props
